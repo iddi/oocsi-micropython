3e63f3be4c1cd1f1295681fb3a38950c7acf5bab
matsfunk <mathias.funk@gmail.com>
2021-09-17 20:41:09 +0200

    - fixed example (missing import)

--- examples/channelSend.py
+++ examples/channelSend.py
@@ -2,6 +2,7 @@
 # This software is released under the MIT License.
 # http://opensource.org/licenses/mit-license.php
 
+import network
 from oocsi import OOCSI
 import time
 from random import random
